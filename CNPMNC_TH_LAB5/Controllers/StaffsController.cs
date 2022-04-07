7e20dae93a1f254b803af70f0f0b0a98660cbee9
Híu Nè <75436344+trughieu@users.noreply.github.com>
2022-04-07 19:39:26 +0700

    Summary

---
+++ CNPMNC_TH_LAB5/Controllers/StaffsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CNPMNC_TH_LAB5.Models;
+
+namespace CNPMNC_TH_LAB5.Controllers
+{
+    public class StaffsController : Controller
+    {
+        private QLNSEntities1 db = new QLNSEntities1();
+
+        // GET: Staffs
+        public ActionResult Index()
+        {
+            var staffs = db.Staffs.Include(s => s.Department1);
+            return View(staffs.ToList());
+        }
+
+        // GET: Staffs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // GET: Staffs/Create
+        public ActionResult Create()
+        {
+            ViewBag.Department = new SelectList(db.Departments, "Department1", "DepartmentName");
+            return View();
+        }
+
+        // POST: Staffs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StaffId,StaffName,Department,StaffMail")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Staffs.Add(staff);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Department = new SelectList(db.Departments, "Department1", "DepartmentName", staff.Department);
+            return View(staff);
+        }
+
+        // GET: Staffs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Department = new SelectList(db.Departments, "Department1", "DepartmentName", staff.Department);
+            return View(staff);
+        }
+
+        // POST: Staffs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StaffId,StaffName,Department,StaffMail")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Department = new SelectList(db.Departments, "Department1", "DepartmentName", staff.Department);
+            return View(staff);
+        }
+
+        // GET: Staffs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Staff staff = db.Staffs.Find(id);
+            db.Staffs.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CNPMNC_TH_LAB5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CNPMNC_TH_LAB5.MvcApplication" Language="C#" %>
